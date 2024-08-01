e33512c6d647b3d5242d1fe69e8a6f57f3565de0
Karishma's Classes <156506602+karishma1900@users.noreply.github.com>
2024-08-01 13:59:26 +0530

    Add files via upload

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+       <div className="header-contents">
+        <h2>Order You Favourate Food Here</h2>
+        <p>choose from a diverse menu featuring a delectable array of dishes Crafted with the finest ingredients and culinary expertise. Our Meal Satisfy Your cravings and elevate your dining experience , one delicious meal at a time.</p>
+        <button>View Menu</button>
+       </div>
+    </div>
+  )
+}
+
+export default Header
